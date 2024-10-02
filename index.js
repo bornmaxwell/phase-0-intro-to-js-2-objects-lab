1725f4768e9039d05b6ba28f7aebe3d77b0e3a49
Maxwell Born <borncmaxwell@gmail.com>
2024-10-02 12:43:47 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Returns a new employee object with the updated key-value pair
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Updates the employee object directly
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    // Create a clone of the employee object
+    let newEmployee = { ...employee };
+    // Delete the key from the new object
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the key directly from the employee object
+    delete employee[key];
+    return employee;
+}
+// Define the employee object
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+};
